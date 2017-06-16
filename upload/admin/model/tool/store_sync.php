71c9a46dfd2c87a87993d37a136164c1a881e6bd
Nathaniel M. Capule <ncapule@google.com>
2017-06-16 21:29:38 +0800

    Add error handler to syncquantity

--- upload/admin/model/tool/store_sync.php
+++ upload/admin/model/tool/store_sync.php
@@ -280,10 +280,16 @@ class ModelToolStoreSync extends Model {
   // 3. Update lazada product quantity (savequantity)
   public function syncquantity($userid, $apikey, $sku, $ocquantity, $lzquantity) {
     // Updates both lazada and cached lazada.
-    $this->savequantity($userid, $apikey, $sku, $lzquantity, $ocquantity);
+    $ret = $this->savequantity($userid, $apikey, $sku, $lzquantity, $ocquantity);
 
     // Update opencart quantity.
-    $this->db->query("UPDATE  " . DB_PREFIX . "product SET quantity = '".(int)$ocquantity."' WHERE model = '".$sku."'");
+    if (isset($ret['ErrorResponse'])) {
+      error_log(print_r($ret['ErrorResponse'], true));
+    } else {
+      $this->db->query("UPDATE  " . DB_PREFIX . "product SET quantity = '".(int)$ocquantity."' WHERE model = '".$sku."'");
+    }
+
+    return $ret;
   }
 
   public function sync($userid, $apikey) {
